b3d7df51ca93e61591c22b565a0dddbf867f942e
kylechang <s4065070@stud.uni-frankfurt.de>
2023-01-26 12:26:25 +0100

    UserDbHandler

--- project/src/main/java/project/userManagement/UserDbHandler.java
+++ project/src/main/java/project/userManagement/UserDbHandler.java
@@ -1,7 +1,10 @@
 package project.userManagement;
 
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 import project.database.MongoDBHandler;
 
 import static com.mongodb.client.model.Filters.eq;
@@ -28,15 +31,29 @@ public class UserDbHandler {
     }
 
     public void replaceUser(Document user) {
-        // not finished yet
+        // get id from input document user
+        String userId = user.getString("_id");
+        // filter the field "_id" with userId
+        Bson filter = Filters.in("_id", userId);
+        //the user is updated with the new Document
+        this.getCollection().updateOne(filter, user);
     }
 
     public void deleteUser(String id) {
-        // not finished yet
+        // create a filter by _id
+        Bson filter = Filters.in("_id", id);
+        // delete the document from DB
+        this.getCollection().deleteOne(filter);
+
     }
 
     public boolean userExists(String id) {
-        return false; // not finished yet
+        // create a filter by _id
+        Bson filter = Filters.in("_id", id);
+        // search the id in the collection
+        FindIterable<Document> search = getCollection().find(filter);
+        // check if the id exists in the collection
+        return search.first() != null;
     }
 
 }
